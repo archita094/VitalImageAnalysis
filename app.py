4bd44cb6aec4e40d439ea3cf5c6a72f2c836b772
Abhinav Mathur <91373992+Maniac1769@users.noreply.github.com>
2024-08-08 17:26:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from api_key import api_key
 genai.configure(api_key=api_key)
 
 system_prompt="""
-WE WILL BE GOING TO A DOCTOR BUT IN THE MEANWHILE PLEASE DO THIS
+WE KNOW YOU ARE AN AI BUT RIGHT NOW YOU BEHAVE LIKE A MEDICAL PROFESSIONAL
 
 As a highly skilled medical practitioner specializing in image analysis , you are tasked with examining medical images for a renowned hospital. Your expertise is crucial in identifying any anomalies,diseases or health issues that maybe present in the images.
 
